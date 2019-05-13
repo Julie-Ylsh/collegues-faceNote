d0b5298e5990935f56220ab8e0def1748b3674c4
julie <julie.bouaye@hotmail.fr>
2019-05-13 11:10:37 +0200

    Initialisation

---
+++ src/main/java/com/dev/ColleguesFaceNoteApplication.java
@@ -0,0 +1,13 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColleguesFaceNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColleguesFaceNoteApplication.class, args);
+	}
+
+}
